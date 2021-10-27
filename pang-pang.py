da0779b6ccefca09eed9d92a2aef4bd93c4be453
Ludvig Hammarstedt <ludvig.hammarstedt@skola.taby.se>
2021-10-27 11:04:17 +0200

    börja arbeta

--- pang-pang.py
+++ pang-pang.py
@@ -1,11 +1,12 @@
 class Player():
     def __init__(self, lifes):
-        self.lifes = self.lifes
+        self.lifes = lifes
         self.scores = 0
         self.did_hit = False
         self.is_hitted = False
 
     def fire(self):
+        print("hello")
         pass
        # Här sker "eldväxlingen"
 
@@ -14,9 +15,12 @@ class Player():
        # Här ska poängen öka
 
     def reduce_lifes(self):
+        self.lifes= self.lifes - 1
         pass
         # Här ska antalet liv minska
 
 
 a_player = Player(3)       # Initiera en spelare med tre liv
 a_player.fire()            # Spelaren skjuter
+a_player.reduce_lifes()
+print(a_player.lifes)
